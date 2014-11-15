97cd8efd66a43ef4401a61d454f7f903a7116858
Kavaskar <kavaskar84@gmail.com>
2014-11-14 23:30:45 -0500

    Initial Check-in
    Sheetal and prasanna Initial work

---
+++ scripts/app/post-option-screen.js
@@ -0,0 +1,107 @@
+/**
+ * Created by mkale on 14/11/2014.
+ */
+/**
+ * Activities view model
+ */
+
+var app = app || {};
+
+app.PostTypes = (function () {
+    'use strict'
+
+    // Activities model
+    var activitiesModel = (function () {
+
+        var activityModel = {
+
+            id: 'Id',
+            fields: {
+                CreatedAt: {
+                    field: 'CreatedAt',
+                    defaultValue: new Date()
+                },
+                TypeName: {
+                    fields: 'TypeName',
+                    defaultValue: null
+                }
+            },
+            CreatedAtFormatted: function () {
+
+                return app.helper.formatDate(this.get('CreatedAt'));
+            },
+            isVisible: function () {
+                var currentUserId = app.Users.currentUser.data.Id;
+                var userId = this.get('UserId');
+
+                return currentUserId === userId;
+            }
+        };
+
+        // Activities data source. The Backend Services dialect of the Kendo UI DataSource component
+        // supports filtering, sorting, paging, and CRUD operations.
+        var activitiesDataSource = new kendo.data.DataSource({
+            type: 'everlive',
+            schema: {
+                model: activityModel
+            },
+            transport: {
+                // Required by Backend Services
+                typeName: 'PostTypes'
+            },
+            change: function (e) {
+
+                if (e.items && e.items.length > 0) {
+                    $('#no-activities-span').hide();
+                } else {
+                    $('#no-activities-span').show();
+                }
+            },
+            sort: { field: 'CreatedAt', dir: 'desc' },
+            selectable:true
+        });
+
+        return {
+            activities: activitiesDataSource
+        };
+
+    }());
+
+    // Activities view model
+    var activitiesViewModel = (function () {
+
+        // Navigate to activityView When some activity is selected
+        var typeSelected = function () {
+            app.mobileApp.navigate('views/BestBuy-Screen.html');
+        };
+
+        // Navigate to app home
+        var navigateHome = function () {
+
+            app.mobileApp.navigate('#welcome');
+        };
+
+        // Logout user
+        var logout = function () {
+
+            app.helper.logout()
+                .then(navigateHome, function (err) {
+                    app.showError(err.message);
+                    navigateHome();
+                });
+        };
+
+        return {
+            activities: activitiesModel.activities,
+            typeSelected: typeSelected,
+            logout: logout
+        };
+
+    }());
+
+    return activitiesViewModel;
+
+}());
+
+
+
