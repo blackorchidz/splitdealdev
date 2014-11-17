7471dbbe892022c6d9075d145d43388b7783cee0
sheetalr <sheetal@blackorchidz.com>
2014-11-17 14:20:47 +0530

    map module

--- scripts/app/settings.js
+++ scripts/app/settings.js
@@ -19,7 +19,7 @@ var appSettings = {
     },
 
     facebook: {
-        appId: '1408629486049918', // Put your Facebook App ID here
+        appId: 'AIzaSyBB0w8xqqbg1CdFkP2QUTF54iUEBK860fk', // Put your Facebook App ID here
         redirectUri: 'https://www.facebook.com/connect/login_success.html' // Put your Facebook Redirect URI here
     },
 
