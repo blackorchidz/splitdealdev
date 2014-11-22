dd9f12b179aa5db5a1d298b85b4f55598b74a201
sheetalr <sheetal@blackorchidz.com>
2014-11-22 15:37:12 +0530

    Revert "post title"
    This reverts commit acb37427521114160a60d5c7f3782685849fd11f.

--- scripts/app/best-buy.js
+++ scripts/app/best-buy.js
@@ -55,7 +55,7 @@ function savePost() {
     });//end of data source
 
     var itemsToInsert = {
-        Title:$('#postTitle').val(),
+        Title:$('#newTitle').val(),
         PostCategory:$('#postCategory').val(),
         Description:$('#postDescription').val(),
         SaleEndDate:$('#saleEndDate').val(),
@@ -69,7 +69,7 @@ function savePost() {
     savePostDS.add(itemsToInsert);
     savePostDS.sync();
 
-    $('#postTitle').val('');
+    $('#newTitle').val('');
     $('#postCategory').val('');
     $('#postDescription').val('');
     $('#saleEndDate').val('');
