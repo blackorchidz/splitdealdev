0c7a8f531a4146b06f17f4ed84d20c95c34abe5f
Rahul Ahuja <ahuja.rahul05@gmail.com>
2015-01-16 15:05:26 +0530

    files updated : best buy(html and js)
    dealing with file id code updated in best buy(js) file. and function
    call in best buy(html) file.

--- scripts/app/best-buy.js
+++ scripts/app/best-buy.js
@@ -97,3 +97,79 @@ function savePost() {
 
 }//end of function
 
+function getDetails()
+{
+
+    $.ajax({
+        type: "GET",
+        url: 'http://api.everlive.com/v1/IMregDJC77R1b1yM/Files',
+        contentType: "application/json",
+        success: function(data){
+
+            var lastId;
+            var fileId;
+            console.log(JSON.stringify(data.Result));
+            alert(JSON.stringify(data.Result));
+            console.log(JSON.stringify(data.Count));
+
+            var obj = JSON.stringify(data.Result);
+
+            console.log("obj "+obj);
+
+            parseData = JSON.parse(obj);
+
+            console.log("After parsing "+parseData);
+            console.log("After parsing length is "+((parseData.length)+1));
+
+            for(i=0; i<parseData.length; i++)
+            {
+                //var fileId  = parseData[i];
+                lastId = parseData[i].Id;
+                console.log("After parsing "+parseData[i].Id);
+                //index = (parseData.length);
+                //console.log("Last ID "+index-1);
+                //console.log("Last ID "+((parseData.length)-1));
+            }
+        
+
+            var contentData = el.data('PostX');
+            var query = new Everlive.Query();
+            query.select('Id');
+            contentData.get(query) // filter
+            .then(function(data){
+                alert(JSON.stringify(data));
+                
+                for(i=0; i<data.result.length; i++)
+                {
+                    fileId = data.result[i].Id;
+                }
+
+                console.log("Last ID "+lastId);
+                console.log("contentData "+fileId);
+
+            // Code Of Association Here
+
+            var item = {
+                Id: fileId,
+                'Picture': lastId
+            };
+
+            var associateData = el.data('PostX');
+            
+            associateData.updateSingle(item,
+                function(data){
+                    alert(JSON.stringify(data));
+                },
+                function(error){
+                    alert(JSON.stringify(error));
+                });
+            },
+                function(error){
+                        alert(JSON.stringify(error));
+                    });
+            },
+                error: function(error){
+                    alert(JSON.stringify(error));
+            }
+        })
+    }
